b3b44e02adf2941ee6a6428d13501e1d07e2a401
Dmitriu Vasilev <mr.disproziu91@gmail.com>
2018-03-12 12:26:59 +0300

    Add Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,33 +1,41 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import toggleOpen from '../../decorators/toggleOpen'
 
-const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Gatsby
-        </Link>
-      </h1>
+import Logo from './headmade.png'
+import './Header.sass'
+
+const Header = ({headerList, isOpen, toggleOpen}) => (
+  <header className='header'>
+    <div className='row'>
+      <div className='columns'>
+        <nav className="header__nav">
+          <div className="nav__logo">
+            <Link className="nav__linkLogo" to="/">
+              <img src={Logo} alt="Logo"/>
+            </Link>
+          </div>
+          <ul className={`nav__list ${isOpen ? 'nav__list--active' : ''}`}>
+            {headerList.map(headerItem =>
+              <li className="nav__item">
+                <Link className="nav__link" to={headerItem.linkPath}>
+                  {headerItem.linkName}
+                </Link>
+              </li>
+            )}
+          </ul>
+          <div className="hamburger-wrapper">
+            <button
+              className={`c-hamburger c-hamburger--htx ${isOpen ? 'active' : ''}`}
+              onClick={toggleOpen}
+            >
+              <span>toggle men</span>
+            </button>
+          </div>
+        </nav>
+      </div>
     </div>
-  </div>
+  </header>
 )
 
-export default Header
+export default toggleOpen(Header)
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,7 +3,13 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
 import Header from '../components/Header'
-import './index.css'
+import '../style/general.scss'
+
+const headerList = [
+  {linkPath: '/projects', linkName: 'Проекты'},
+  {linkPath: '/contacts', linkName: 'Контакты'},
+  {linkPath: '/about', linkName: 'О нас'},
+]
 
 const TemplateWrapper = ({ children }) => (
   <div>
@@ -14,15 +20,8 @@ const TemplateWrapper = ({ children }) => (
         { name: 'keywords', content: 'sample, something' },
       ]}
     />
-    <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
+    <Header headerList={headerList} />
+    <div>
       {children()}
     </div>
   </div>
